db2d99b48a49ad6ab374f0749be539ae6f740f58
Ryan Kirsch <agalloch88@gmail.com>
2022-02-07 16:22:11 -0500

    list out menu items, prep to style menu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./Header";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
 function App() {
+  const [isMenuOpen, setisMenuOpen] = useState(false);
+
   return (
-    <div className="App">
-      <h1>Tesla Site </h1>
-      {/* header */}
-      {/* header block */}
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        {/* header */}
+        {/* header block */}
+      </div>
+    </Router>
   );
 }
 
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
-      <div className="header__logo"></div>
+      <div className="header__logo">
+        <Link to="/">
+          <img
+            className="header__logoImg"
+            src="https://assets.website-files.com/5e8fceb1c9af5c3915ec97a0/5ec2f037975ed372da9f6286_Tesla-Logo-PNG-HD.png"
+            alt="Tesla styled logo"
+          />
+        </Link>
+      </div>
+      <div className="header__links">
+        <Link to="/">Model S</Link>
+        <Link to="/">Model 3</Link>
+        <Link to="/">Model X</Link>
+        <Link to="/">Model Y</Link>
+        <Link to="/">Solar Roof</Link>
+        <Link to="/">Solar Panels</Link>
+      </div>
+      <div className="header__right">
+          <Link to="/">Shop</Link>
+          <Link to="/">Account</Link>
+          <div className="header__menu">
+              Menu
+          </div>
+      </div>
     </div>
   );
 }
