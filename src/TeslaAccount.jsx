832f6c339a6513764f2b969bf9abc5e3773493f7
Ryan Kirsch <agalloch88@gmail.com>
2022-02-15 11:59:04 -0500

    create logoutOfApp functionality, test persistence

--- src/TeslaAccount.jsx
+++ src/TeslaAccount.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import "./TeslaAccount.css";
 import { Link, useHistory } from "react-router-dom";
-import CloseIcon from '@material-ui/icons/Close';
-import { useSelector } from "react-redux";
-import { selectUser } from "./features/userSlice";
+import CloseIcon from "@material-ui/icons/Close";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, selectUser } from "./features/userSlice";
 import Car from "./Car";
+import { auth } from "./firebase";
 
-function TeslaAccount({isMenuOpen, setIsMenuOpen}) {
-    const user = useSelector(selectUser)
-    const logoutOfApp = () => {
+function TeslaAccount({ isMenuOpen, setIsMenuOpen }) {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-    }
+  const logoutOfApp = () => {
+    auth
+      .signOut()
+      .then(() => {
+        dispatch(logout());
+        history.push("/");
+      })
+      .catch((error) => alert(error.message));
+  };
 
   return (
     <div className="teslaAccount">
@@ -34,27 +44,39 @@ function TeslaAccount({isMenuOpen, setIsMenuOpen}) {
           <Link to="/tesla-account">Shop</Link>
           <Link to="/tesla-account">Account</Link>
           <Link onClick={logoutOfApp}>Log Out</Link>
-          <div className="teslaAccount__menu" onClick={() => setIsMenuOpen(!isMenuOpen)}>
-          {isMenuOpen ? <CloseIcon className="teslaAccount__closeMenu"/> : "Menu"}
-
+          <div
+            className="teslaAccount__menu"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            {isMenuOpen ? (
+              <CloseIcon className="teslaAccount__closeMenu" />
+            ) : (
+              "Menu"
+            )}
           </div>
         </div>
       </div>
       <div className="teslaAccount__info">
-          <div className="teslaAccount__person">
-              <h4>{user?.displayName + "'s Tesla"}</h4>
-          </div>
-          <div className="teslaAccount__infoRight">
-              <Link>Home</Link>
-              <Link>Account</Link>
-              <Link>History</Link>
-              <Link onClick={logoutOfApp}>Log Out</Link>
-          </div>
+        <div className="teslaAccount__person">
+          <h4>{user?.displayName + "'s Tesla"}</h4>
+        </div>
+        <div className="teslaAccount__infoRight">
+          <Link>Home</Link>
+          <Link>Account</Link>
+          <Link>History</Link>
+          <Link onClick={logoutOfApp}>Log Out</Link>
+        </div>
       </div>
       <div className="teslaAccount__car">
-          <Car imgSrc="https://www.tesla.com/tesla_theme/assets/img/mytesla/v3/header-nocar-models@2x.jpg?20170815" model="model s" testDrive />
-          <Car imgSrc="https://www.tesla.com/tesla_theme/assets/img/mytesla/v3/header-nocar-modelx@2x.jpg?20170815" model="model x" />
-
+        <Car
+          imgSrc="https://www.tesla.com/tesla_theme/assets/img/mytesla/v3/header-nocar-models@2x.jpg?20170815"
+          model="model s"
+          testDrive
+        />
+        <Car
+          imgSrc="https://www.tesla.com/tesla_theme/assets/img/mytesla/v3/header-nocar-modelx@2x.jpg?20170815"
+          model="model x"
+        />
       </div>
     </div>
   );
