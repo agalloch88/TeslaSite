8cc8e9f689f2c0227a650cd6d11ba9dc7d04a261
Ryan Kirsch <agalloch88@gmail.com>
2022-02-18 06:21:40 -0500

    break out underline section to separate styled div

--- src/Section.jsx
+++ src/Section.jsx
@@ -1,31 +1,41 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-function Section({backgroundImage, title, description,}) {
+function Section({ backgroundImage, title, description, DeliveryText }) {
   return (
     <Wrap bgImage={backgroundImage}>
-        <ItemText>
-            <h1>{title}</h1>
-            <p>{description}</p>
-        </ItemText>
+      <ItemText>
+        <h1>{title}</h1>
+        <p>
+          {description}
+          <DeliverySpan>
+            <span>{DeliveryText}</span>
+          </DeliverySpan>
+        </p>
+      </ItemText>
     </Wrap>
-  )
+  );
 }
 
 export default Section;
 
 const Wrap = styled.div`
-    background-image: ${(props) => `url("./images/${props.bgImage}")`};
-    background-position: center;
-    background-repeat: no-repeat;
-    -webkit-background-size: cover;
-    -moz-background-size: cover;
-    -o-background-size: cover;
-    background-size: cover;
-    height: 100vh;
-    width: 100vw;
-`
+  background-image: ${(props) => `url("./images/${props.bgImage}")`};
+  background-position: center;
+  background-repeat: no-repeat;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  height: 100vh;
+  width: 100vw;
+`;
 
 const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const DeliverySpan = styled.div`
     
 `
